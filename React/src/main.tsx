fe69354f5a920f0d77e60c087972a73bf17183dd
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-11 21:35:15 +0300

    Fix DevExtreme licensing import order

--- React/src/main.tsx
+++ React/src/main.tsx
@@ -1,13 +1,12 @@
-import config from 'devextreme/core/config';
-import { licenseKey } from './devextreme-license';
-
-config({ licenseKey });
-
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import 'devextreme/dist/css/dx.material.blue.light.compact.css';
+import config from 'devextreme/core/config';
 import './index.css';
 import App from './App.tsx';
+import { licenseKey } from './devextreme-license';
+
+config({ licenseKey });
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
